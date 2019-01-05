d3eb6e8fdb6d5f4c41fad4d69d6474ef9f37baf8
Josue Neis <josue@socialcondo.com.br>
2019-01-05 11:56:53 -0200

    review basics

--- localusers/luser-demo01.sh
+++ localusers/luser-demo01.sh
@@ -2,100 +2,96 @@
 
 # #!<interpreter-path>
 #  
-#  - shebang tells which interpreter to use
-#    when running commands inside the script 
+#  Shebang tells which interpreter to use
+#    when running commands inside the script.
 #
-#  - e.g. #!/usr/bin/python
+#  E.g.
+#    $ #!/usr/bin/python
 #  
-#  - equivalent to
-#    > /bin/bash <script-file>
+#  Equivalent to
+#    $ /bin/bash <script-file>
 
-# permissions
+# File permissions.
 #
-#  - drwxrwxrwx
-#    - d directory or - file
-#    - r read, w write, x execute, - none
-#    - first group is owner permissions
-#    - second group is group permissions
-#    - last group is other user's permission
+#  drwxrwxrwx
+#    d directory or - file.
+#    r read, w write, x execute, - none.
+#    First group is owner permissions.
+#    Second group is group permissions.
+#    Last group is other user's permission.
 #  
-#  - e.g.
-#    > ls -l
-#    $ -rwxr-xr-x  1 josue staff 1143 Dec 7 21:38 luser-demo01.sh
+#  E.g.
+#    $ ls -l
+#    -rwxr-xr-x  1 josue staff 1143 Dec 7 21:38 luser-demo01.sh
 
 # chmod rwx <file>
 #
-#  - set permissions to files
+#  Set permissions to files.
 #
-#  - r = 4
-#  - w = 2
-#  - x = 1
-#  - - = 0 (none)
+#  r = 4
+#  w = 2
+#  x = 1
+#  - = 0 (none)
 
-# shell built-in commands
+# Shell built-in commands.
 #
-#  - can be executed directly in the shell,
+#  Can be executed directly in the shell,
 #    without needing to inform the command path or
-#    adding them to the system path
+#    adding them to the system path.
 #
-#  - e.g.
-#    > type -a echo
-#    $ echo is a shell builtin
-#    $ echo is /usr/bin/echo
+#  E.g.
+#    $ type -a echo
+#    echo is a shell builtin
+#    echo is /usr/bin/echo
 
 # man <command>
 #
-#  - displays commands documentation
+#  Displays scripts documentation.
 
-# script variables
+# Script variables.
 #
-#  - can be named with letters, digits and underscore
-#    (not starting with digits)
-#  - naming conventions tell to use uppercase
-#  - are case-sensitive
+#  Can be named with letters, digits and underscore (not starting with digits).
+#  Naming conventions tell to use uppercase.
+#  Are case-sensitive.
 #  
-#  - assignments CANNOT have spaces between
-#    variable, equal sign, and value
+#  Assignments CANNOT have spaces between variable, equal sign, and value.
+#  Variable values can be overriden.
 #
-#  - single quotes prevent variable expansion
+#  Single quotes prevent variable expansion.
 #
-#  - $ is used to reference a variable's value
-#  - ${} must be used when appending content 
-#    to variables inside a string 
-#
+#  $ is used to reference a variable's value.
+#  ${} must be used when appending content to variables inside a string.
+
 WORD='script'
 ending='ed'
-
-# overriding variable values
 ending='ing'
 
 # echo <content>
 #
-#  - prints content to stdout
+#  Prints content to stdout.
 
 echo 'Hello'
-# $ Hello
+# Hello
 
 echo "$WORD"
-# $ script
+# script
 
 echo '$WORD'
-# $ $WORD
+# $WORD
 
 echo "This is a shell $WORD"
-# $ This is a shell script
+# This is a shell script
 echo "This is a shell ${WORD}"
-# $ This is a shell script
+# This is a shell script
 
 echo "${WORD}ing is fun"
-# $ scripting is fun
+# scripting is fun
 
 echo "$WORDing is fun"
-# $  is fun
+#  is fun
 
 echo "This is ${WORD}${ENDING}"
-# $ This is script
+# This is script
 
 echo "${WORD}$ending is fun"
-# $ scripting is fun
-
+# scripting is fun
--- localusers/luser-demo02.sh
+++ localusers/luser-demo02.sh
@@ -1,54 +1,112 @@
 #!/bin/bash
 
-# Display the UID and username of the user executing this script
-# Display if the user is the root or not
+# UID
+# 
+#  Read-only, preset shell variable.
+#  ID of current user.
+#  Root user ID is always 0.
+#
+# $ man bash
+# /UID
+# :n
+# :shift+n
 
-# Display the UID
 echo "Your UID is ${UID}"
-echo "Your EUID is ${EUID}"
+# Your UID is 0
 
-# EUID is almost always equal to UID
-# Some bashes allow scripts to set UID (usually read-only)
-# In such cases, UID and EUID would differ
+# id -un [<user>]
+#
+#  Returns user info.
+#
+#  -u UID
+#  -un user name 
 
-# Using builtin id
 id
+# uid=0(root) gid=0(root) groups=0(root)
+
 id -u
-id -n # id: cannot print only names or real IDs in default format
+# 0
+
+id -n
+# id: cannot print only names or real IDs in default format 
+
 id -u -n
+# root
 
-# Using builtin whoami (same as id -un)
+# whoami
+#
+#  Returns user name.
+#  Same as id -un.
+
 whoami
+# root
 
-# Display the username
+# $()
+# 
+#  Command expressions.
+#  Can be used for, e.g., storing command output into variables.
+#
 USER_NAME=$(id -un)
 echo "Your username is $USER_NAME"
+# Your username is root
 
-# Old syntax for command expressions
-USER_NAME=`id -un`
-echo "Again, your username is $USER_NAME"
+# ``
+#
+#  Equivalent, old syntax for command expressions.
+#
+USER_ID=`id -u`
+echo "Your user ID is $USER_ID"
+# Your user ID is 0
 
-# Display if the user is root (root is always id 0)
+# if [[ <condition> ]]
+# then
+#   <commands>
+# else
+#   <commands>
+# fi
+#
+#  If statement.
+#  
+#  if keyword must be followed by a space.
+#  Condition is enclosed in double brackets.
+#  Opening brackets must be followed by a space.
+#  Closing brackets must be preceeded by a space.
+#  Variables referenced inside condition must be enclosed in double quotes.
+
 if [[ "${UID}" -eq 0 ]]
 then
   echo 'You are root'
 else
   echo 'You are not root'
 fi
+# You are root
 
-# If statements
-# - if keyword must be followed by a space
-# - condition is enclosed in double brackets
-# - opening brackets must be followed by a space
-# - closing brackets must be preceeded by a space
-# - variables referenced inside condition must be enclosed in double quotes
+# [[
+#
+#  Shell built-in command.
+#  Executes a conditional command.
+#  Returns a status of 0 or 1, depending on the
+#    evaluation of the conditional expression inside it.
+#
+# [ 
+#
+#  Equivalent, old syntax.
 
-# New-line characters are command separators
-# Alternatively, semi-colons can be used as command separators for one-line, multi commands
-if [[ "${UID}" -eq 0 ]]; then echo 'You are root'; fi
+#  Logical (short-circuit) operators.
+#
+# &&, ||
 
-# [[ is a builtin command
-# It executes a conditional command
-# Returns a status of 0 or 1 depending on the evaluation of the conditional expression inside it
-# Double brackets are new syntax for old single brackets [
+# Arithmetic binary operators.
+#
+#  -eq, -ne, -lt, -le, -gt, -ge
+# 
+#  ==, !=, <, <=, >, >=
+
+# ;
+#
+#  Alternative command separator for single-line, multi-command statements.
+
+if [[ $(id -un) -ne 0 ]]; then echo 'Not root'; else echo 'You are...'; echo '...root'; fi
+# You are...
+# ...root
 
